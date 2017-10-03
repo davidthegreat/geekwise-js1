bac26edba6be4213b7cdc8bbda08d06116e7b2bb
David Ramirez <davidsramirez11@gmail.com>
2017-10-03 16:47:17 -0700

    add eventlisteners

---
+++ EventListeners/js/script.js
@@ -0,0 +1,57 @@
+// JavaScript Event Listeners 
+// element.addEventListener(event, function)
+// Syntax ^^
+
+// click
+// Event occurs on a complete action of left clicking and releasing.
+
+
+
+// mousedown
+// Event occurs at the moment the left click is pressed.
+
+
+
+//mouseup
+// Event occurs at the moment the left click is released.
+
+
+
+// mousemove
+// Event occurs anytime the mouse is moved.
+// https://www.w3schools.com/jsref/obj_window.asp
+// you will need window object for this ^^
+
+
+// mouseenter
+// Event occurs when the cursor enters the targeted element.
+
+
+
+// mouseleave
+// Event occurs when the cursor leaves the targeted element.
+
+
+
+// scroll
+// Event occurs when the user scrolls the page, either up or down. 
+// Does not fire automatically when the scroll wheel is spun. Only if the page scrolls.
+
+
+
+// keypress
+// Event occurs on the complete action pressing and releasing any key.
+
+
+// keydown
+// Event occurs at the moment any key is pressed.
+
+
+
+// keyup
+// Event occurs at the moment any key is released.
+
+
+
+// submit
+// The submit event is fired when a form is submitted.
