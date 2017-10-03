82bc415d84266ce40ec2633fc55174105f4a60c1
David Ramirez <davidsramirez11@gmail.com>
2017-10-03 16:39:07 -0700

    add EventListners

---
+++ TargetDOMElements/js/script.js
@@ -0,0 +1,38 @@
+// document.getElementById('');
+// Returns a reference to the element by its ID; the ID is a string which can 
+// be used to uniquely identify the element, found in the HTML id attribute.
+
+
+
+
+
+
+
+// document.getElementsByTagName('');
+// Returns an array-like object (HTMLCollection) of elements with the given tag name.
+
+
+
+
+
+
+// document.getElementsByClassName('');
+// Returns an array-like object (HTMLCollection) of all child elements which have all of the given class names.
+
+
+
+
+
+
+// querySelector();
+// Returns the first Element within the document that matches the specified selector, or group of selectors.
+// Use of . for classes and # for ID's apply.
+
+
+
+
+
+// .querySelectorAll();
+// Returns a list (NodeList) of the elements within the document that match the specified group of selectors. Use of . for classes and # for ID's apply.
+
+
